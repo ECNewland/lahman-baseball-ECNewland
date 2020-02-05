b84fe55958e875552aeabd3139274d1c4872e585
ECNewland <ecnewland@mac.com>
2020-02-04 20:23:01 -0600

    Question_2

--- Question_2.sql
+++ Question_2.sql
@@ -1,7 +1,7 @@
 /* Question_2
 Find the name and height of the shortest player in the database.
 How many games did he play in? What is the name of the team for which he played?
-A. Eddie Gaedel, height = 43, 
+A. Eddie Gaedel, height = 43, 1 game, St. Louis Browns.
 
 TABLE NOTES:
 people = SELECT playerid, namefirst, namelast, height.
@@ -9,10 +9,16 @@ teamshalf = JOIN teamid, g for games played
 
 SELECT playerid, namefirst, namelast, MIN(height) AS height, teamid, g
 FROM 
-LEFT JOIN teamshalf
+LEFT JOIN team(teamID, G, name)
 ORDER BY height;
 */
-
+SELECT p.playerid, p.namefirst, p.namelast, p.height, t.name, a.g_all
+FROM people AS p
+INNER JOIN appearances AS a
+USING (playerid)
+JOIN teams AS t
+USING (teamid)
+ORDER BY height ASC;
 
 
 /* Question_1
